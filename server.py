77a87ca7e7a0835fb829b7f03ec02179f2248a3a
Luybov <kalna.lyubov2017@yandex.ru>
2026-05-05 14:08:23 +0300

    поправила все файлы, убрала лишние записи

--- server.py
+++ server.py
@@ -8,16 +8,9 @@ serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """
-           Обрабатывает входящие GET-запросы от клиента.
-
-           В зависимости от URL:
-           - возвращает HTML-страницы сайта
-           - или отдает статические файлы (например, изображения)
-           """
+        """Обрабатывает входящие GET-запросы от клиента."""
         path = self.path
 
-        # 🔹 Обработка картинки
         if path.endswith(".jpg"):
             try:
                 with open(path[1:], "rb") as file:
@@ -25,16 +18,12 @@ class MyServer(BaseHTTPRequestHandler):
                     self.send_header("Content-type", "image/jpeg")
                     self.end_headers()
                     self.wfile.write(file.read())
-            except:
+            except FileNotFoundError:
                 self.send_error(404)
             return
-
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-
-
-        # 🔥 Роутинг (куда переходить)
         if path == "/" or path == "/index":
             filename = "index.html"
         elif path == "/catalog":
@@ -44,47 +33,37 @@ class MyServer(BaseHTTPRequestHandler):
         elif path == "/contacts":
             filename = "contacts.html"
         else:
-            filename = "contacts.html"  # на любой другой запрос
-
+            filename = "contacts.html"
         with open(filename, "r", encoding="utf-8") as file:
             html = file.read()
-
         self.wfile.write(bytes(html, "utf-8"))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        """Обрабатывает POST-запросы"""
+        content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
-
-        # 👉 декодируем строку
         data = post_data.decode("utf-8")
-
-        # 👉 превращаем в словарь
         parsed_data = parse_qs(data)
-
-        print("\n📩 Получены данные формы:")
+        print("\nПолучены данные формы:")
 
         for key, value in parsed_data.items():
             print(f"{key}: {value[0]}")
 
-        # ответ клиенту
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
         with open("contacts.html", "r", encoding="utf-8") as file:
             html = file.read()
-
         self.wfile.write(bytes(html, "utf-8"))
 
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
-
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
     webServer.server_close()
     print("Server stopped.")
